2ac9672365ff9d1c5255106d28170b76d9213d63
qamarelsafadi <qamar.elsafadi98@gmail.com>
2022-11-01 19:11:48 +0200

    feat(rating-bar): implement rating bar part 1

--- app/src/main/java/com/qamar/composablewidgets/MainActivity.kt
+++ app/src/main/java/com/qamar/composablewidgets/MainActivity.kt
@@ -6,23 +6,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.qamar.composablewidgets.ui.dragexample.DragExample
-import com.qamar.composablewidgets.ui.lottieexample.LottieExample
-import com.qamar.composablewidgets.ui.menu.MenuExample
-import com.qamar.composablewidgets.ui.modifiers.TryModifiers
+import androidx.compose.ui.unit.dp
+import com.qamar.composablewidgets.ui.ratingbar.RatingBar
 import com.qamar.composablewidgets.ui.sendemail.SendEmailExample
-import com.qamar.composablewidgets.ui.sideeffect.TryLaunchedEffect
-import com.qamar.composablewidgets.ui.staggered.StaggeredList
-import com.qamar.composablewidgets.ui.state.StateExample
-import com.qamar.composablewidgets.ui.swipableitem.SwipeList
-import com.qamar.composablewidgets.ui.swipetorefresh.SwipeToRefreshList
 import com.qamar.composablewidgets.ui.theme.ComposableWidgetsTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,10 +33,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview()
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposableWidgetsTheme {
-        SendEmailExample()
+        RatingBar(rating = 3.7f, gap = 3.dp)
     }
 }
---
+++ app/src/main/java/com/qamar/composablewidgets/ui/ratingbar/RatingBar.kt
@@ -0,0 +1,104 @@
+package com.qamar.composablewidgets.ui.ratingbar
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.qamar.composablewidgets.R
+
+@Composable
+ fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Float,
+    gap: Dp = 0.dp,
+) {
+
+    val image = ImageBitmap.imageResource(id = R.drawable.ic_baseline_star_border_24)
+    val imageFull = ImageBitmap.imageResource(id = R.drawable.ic_baseline_star_24)
+
+    val totalCount = 5
+
+    val height = LocalDensity.current.run { image.height.toDp() }
+    val width = LocalDensity.current.run { image.width.toDp() }
+    val space = LocalDensity.current.run { gap.toPx() }
+    val totalWidth = width * totalCount + gap * (totalCount - 1)
+
+
+    Box(
+        modifier
+            .width(totalWidth)
+            .height(height)
+            .drawBehind {
+                drawRating(rating, image, imageFull, space)
+            })
+}
+
+private fun DrawScope.drawRating(
+    rating: Float,
+    image: ImageBitmap,
+    imageFull: ImageBitmap,
+    space: Float
+) {
+
+    val totalCount = 5
+
+    val imageWidth = image.width.toFloat()
+    val imageHeight = size.height
+
+    val reminder = rating - rating.toInt()
+    val ratingInt = (rating - reminder).toInt()
+
+    for (i in 0 until totalCount) {
+
+        val start = imageWidth * i + space * i
+
+        drawImage(
+            image = image,
+            topLeft = Offset(start, 0f)
+        )
+    }
+
+    drawWithLayer {
+        for (i in 0 until totalCount) {
+            val start = imageWidth * i + space * i
+            // Destination
+            drawImage(
+                image = imageFull,
+                topLeft = Offset(start, 0f)
+            )
+        }
+
+        val end = imageWidth * totalCount + space * (totalCount - 1)
+        val start = rating * imageWidth + ratingInt * space
+        val size = end - start
+
+        // Source
+        drawRect(
+            Color.Transparent,
+            topLeft = Offset(start, 0f),
+            size = Size(size, height = imageHeight),
+            blendMode = BlendMode.SrcIn
+        )
+    }
+}
+
+private fun DrawScope.drawWithLayer(block: DrawScope.() -> Unit) {
+    with(drawContext.canvas.nativeCanvas) {
+        val checkPoint = saveLayer(null, null)
+        block()
+        restoreToCount(checkPoint)
+    }
+}
