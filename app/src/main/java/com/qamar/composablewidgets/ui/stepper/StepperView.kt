8172bb1dc193b33af265b5efd11450a3bc35fb8c
qamarelsafadi <qamar.elsafadi98@gmail.com>
2023-01-13 00:05:53 +0200

    fix: delete unused imports

--- app/src/main/java/com/qamar/composablewidgets/ui/stepper/StepperView.kt
+++ app/src/main/java/com/qamar/composablewidgets/ui/stepper/StepperView.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.qamar.composablewidgets.R
-import com.qamar.composablewidgets.figmacard.Image
 
 @Preview
 @Composable
