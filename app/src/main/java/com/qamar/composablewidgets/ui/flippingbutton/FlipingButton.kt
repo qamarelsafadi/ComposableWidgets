b0390d6647fac6b70828677ee2819d994700be9c
qamarelsafadi <qamar.elsafadi98@gmail.com>
2023-01-16 19:45:15 +0200

    feat: flipping button

---
+++ app/src/main/java/com/qamar/composablewidgets/ui/flippingbutton/FlipingButton.kt
@@ -0,0 +1,80 @@
+package com.qamar.composablewidgets.ui.flippingbutton
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+
+enum class FlipFace(val angle: Float) {
+    Front(0f) {
+        override val next: FlipFace
+            get() = Back
+    },
+    Back(180f) {
+        override val next: FlipFace
+            get() = Front
+    };
+
+    abstract val next: FlipFace
+}
+
+enum class RotationAxis {
+    AxisX,
+    AxisY,
+}
+
+
+@ExperimentalMaterialApi
+@Composable
+fun FlipButton(
+    cardFace: FlipFace,
+    modifier: Modifier = Modifier,
+    axis: RotationAxis = RotationAxis.AxisY,
+    back: @Composable () -> Unit = {},
+    front: @Composable () -> Unit = {},
+) {
+    val rotation = animateFloatAsState(
+        targetValue = cardFace.angle,
+        animationSpec = tween(
+            durationMillis = 400,
+            easing = FastOutSlowInEasing,
+        )
+    )
+    Box(modifier = modifier
+            .graphicsLayer {
+                if (axis == RotationAxis.AxisX) {
+                    rotationX = rotation.value
+                } else {
+                    rotationY = rotation.value
+                }
+                cameraDistance = 12f * density
+            },
+    ) {
+        if (rotation.value <= 90f) {
+            Box(
+                Modifier.fillMaxSize()
+            ) {
+                front()
+            }
+        } else {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .graphicsLayer {
+                        if (axis == RotationAxis.AxisX) {
+                            rotationX = 180f
+                        } else {
+                            rotationY = 180f
+                        }
+                    },
+            ) {
+                back()
+            }
+        }
+    }
+}
