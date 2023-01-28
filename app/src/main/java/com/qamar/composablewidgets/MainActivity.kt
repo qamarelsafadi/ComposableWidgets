288fff00b06389a38fc81624ddbd23bfb9ef194a
qamarelsafadi <qamar.elsafadi98@gmail.com>
2023-01-28 16:51:11 +0200

    fix: round loading with white box

--- app/src/main/java/com/qamar/composablewidgets/MainActivity.kt
+++ app/src/main/java/com/qamar/composablewidgets/MainActivity.kt
@@ -8,9 +8,13 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
@@ -26,32 +30,29 @@ import com.qamar.composablewidgets.ui.theme.ComposableWidgetsTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.qamar.composablewidgets.ui.MyButton
+import com.qamar.composablewidgets.ui.aysncimage.AsyncImage
+import com.qamar.composablewidgets.ui.theme.Bg
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposableWidgetsTheme {
-                var otpValue by remember {
-                    mutableStateOf("")
+                Box(Modifier.fillMaxSize()
+                    .background(Color.LightGray),
+                contentAlignment = Alignment.Center) {
+                    AsyncImage(
+                        imageUrl = "https://developer.android.com/static/codelabs/jetpack-compose-animation/img/5bb2e531a22c7de0.png",
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(64.dp)
+                    )
                 }
-                MyButton(
-                    text = "My Custom Button",
-                    backgroundColor = Color.Blue,
-                    roundedRadius = 8.dp,
-                    leadingIcon = {
-                        Icon(Icons.Filled.Star, contentDescription = null)
-                    },
-                    trailingIcon = {
-                        Icon(Icons.Filled.Star, contentDescription = null)
-                    },
-                    onClick = {
-                        // Perform action when button is clicked
-                    }
-                )
+
             }
         }
     }
--- app/src/main/java/com/qamar/composablewidgets/ui/aysncimage/AsyncImage.kt
+++ app/src/main/java/com/qamar/composablewidgets/ui/aysncimage/AsyncImage.kt
@@ -1,11 +1,17 @@
 package com.qamar.composablewidgets.ui.aysncimage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -21,18 +27,27 @@ fun AsyncImage(
     = Modifier
         .size(90.dp)
         .padding(horizontal = 8.dp),
-    contentScale: ContentScale = ContentScale.Fit
+    contentScale: ContentScale = ContentScale.Crop
 ) {
     SubcomposeAsyncImage(
         modifier = modifier,
         model = imageUrl,
         loading = {
-            LottieAnimationSizeView(
-                raw = R.raw.imageloading,
+            Box(
                 modifier = Modifier
-                    .width(25.dp)
-                    .height(25.dp)
-            )
+                    .clip(CircleShape)
+                    .background(Color.White)
+                    .size(64.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                LottieAnimationSizeView(
+                    raw = R.raw.imageloading,
+                    modifier = Modifier
+                        .width(25.dp)
+                        .height(25.dp)
+                )
+            }
+
         },
         contentDescription = "",
         contentScale = contentScale
